67a9212512828d16596c835a32b0ccdf7647b7e2
Erinç Çakır <erinc.cakir@armongate.com>
2025-10-21 11:59:48 +0300

    sources removed

--- Sources/ArmongateMobileAccessSDK/ArmongateMobileAccessSDK.swift
+++
@@ -1,9 +0,0 @@
-// ArmongateMobileAccessSDK - Source Wrapper
-// This file re-exports the binary framework and VoramCore dependency
-
-@_exported import ArmongateMobileAccessSDKBinary
-@_exported import VoramCore
-
-// Re-export all public APIs from the binary framework
-// This ensures both ArmongateMobileAccessSDK and VoramCore are available
-// when importing just ArmongateMobileAccessSDK
--- Sources/ArmongateMobileAccessSDKWrapper/ArmongateMobileAccessSDKWrapper.swift
+++
@@ -1,3 +0,0 @@
-// Re-export ArmongateMobileAccessSDK
-@_exported import ArmongateMobileAccessSDK
-@_exported import VoramCore
